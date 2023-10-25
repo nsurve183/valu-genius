ab9f3a94746e7a40e33d2672ccc3ac02e56df93e
nsurve183 <nsurve183@gmail.com>
2023-10-25 12:48:20 +0530

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import './Scss/main.css'
+
+function App() {
+  return (
+    <>
+      <Router>
+        <h1 className='text'>Hello World</h1>
+        <h2 className='text1'>Hello Nik</h2>
+        <Routes>
+
+        </Routes>
+      </Router>
+    </>
+  )
+}
+
+export default App
